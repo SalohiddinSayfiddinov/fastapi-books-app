cc0bccd174216ea0b88dc38fa012255f16c46f75
SalohiddinSayfiddinov <salokhiddin.sayfiddinov@mail.ru>
2025-06-26 09:26:10 +0500

    cors allowed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from auth.routes import router as auth_router
 from categories.routes import router as categories_router
 from books.routes import router as books_router
@@ -6,6 +7,14 @@ from authors.routes import router as authors_router
 from vendors.routes import router as vendors_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_router)
 app.include_router(categories_router)
