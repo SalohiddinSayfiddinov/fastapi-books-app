df5ef129da83d5dfdae53af30a6917270e8be3d8
SalohiddinSayfiddinov <salokhiddin.sayfiddinov@mail.ru>
2025-07-03 09:27:26 +0500

    changes in authors

--- authors/routes.py
+++ authors/routes.py
@@ -6,20 +6,9 @@ from books.models import BOOKS
 router = APIRouter(tags=["authors"])
 
 @router.get("/authors", response_model=List[Author])
-def list_authors():
-    return AUTHORS
-
-@router.get("/authors/{author_id}", response_model=Author)
-def get_author(author_id: int):
-    author = next((a for a in AUTHORS if a.id == author_id), None)
-    if not author:
-        raise HTTPException(status_code=404, detail="Author not found")
-    return author
-
-@router.get("/authors/search/by_name")
-def search_authors(
+def list_authors(
     q: Optional[str] = Query(None, description="Search term for author name or nationality"),
-    genre: Optional[str] = Query(None, description="Filter by genre")
+    genre: Optional[str] = Query(None, description="Filter by genre"),
 ):
     results = AUTHORS
     
@@ -37,7 +26,16 @@ def search_authors(
             if any(g.lower() == genre_lower for g in author.genres)
         ]
     
+  
+    
     return results
+
+@router.get("/authors/{author_id}", response_model=Author)
+def get_author(author_id: int):
+    author = next((a for a in AUTHORS if a.id == author_id), None)
+    if not author:
+        raise HTTPException(status_code=404, detail="Author not found")
+    return author
 
 @router.get("/authors/{author_id}/books")
 def get_author_books(author_id: int):
